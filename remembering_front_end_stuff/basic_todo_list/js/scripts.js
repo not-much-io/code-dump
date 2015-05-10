e735e681f64eecf9c7af5b13b1bdcd33d08ac795
KristoKoert <kristo.koert@gmail.com>
2015-05-10 12:58:49 +0300

    Fixed up basic todo

--- remembering_front_end_stuff/basic_todo_list/js/scripts.js
+++ remembering_front_end_stuff/basic_todo_list/js/scripts.js
@@ -6,12 +6,43 @@ window.onload = function () {
 };
 
 function todoFactory(text) {
-    var li = document.createElement("li");
-    var id = "todo" + (document.getElementById("list-of-todos").getElementsByTagName("li").length + 1).toString();
-    console.log(id);
-    li.innerHTML = text + "<button onclick='removeToDo(this.parentNode)'>-</button>";
-    li.id = id;
-    return li;
+
+    var createContent = function(text) {
+        var content = document.createElement("span");
+        content.innerHTML = text;
+        content.className = "align-left";
+        return content;
+    };
+
+    var createRemoveButton = function() {
+        var removeButton = document.createElement("button");
+        removeButton.innerHTML = "-";
+        removeButton.addEventListener("click", function() {
+            removeToDo(this.parentNode);
+        });
+        removeButton.className = "align-right";
+        return removeButton;
+    };
+
+    var createLi = function(content, removeButton) {
+        var li = document.createElement("li");
+        li.appendChild(content);
+        li.appendChild(removeButton);
+        return li;
+    };
+
+    return createLi(createContent(text), createRemoveButton())
+}
+
+function colorTodos() {
+    var todos = document.getElementById("list-of-todos").getElementsByTagName("li");
+    for (var i = 0; i < todos.length; i++) {
+        if (i % 2 == 0) {
+            todos[i].className = "even-todo";
+        } else {
+            todos[i].className = "odd-todo";
+        }
+    }
 }
 
 function addToDo() {
@@ -21,8 +52,10 @@ function addToDo() {
     var newTodo = todoFactory(newTodoText);
     todoList.appendChild(newTodo);
     inputField.value = "";
+    colorTodos();
 }
 
 function removeToDo(todoListItem) {
     todoListItem.parentNode.removeChild(todoListItem);
+    colorTodos();
 }
